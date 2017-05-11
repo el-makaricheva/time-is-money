cf153d53832218187d5f1fa3d6f2ed37fe8ae9c0
Elena <el.makaricheva@gmail.com>
2017-05-11 23:30:06 +0300

    Added responsive appearance

--- app/js/main.js
+++ app/js/main.js
@@ -20,10 +20,42 @@ $(function() {
 		event.preventDefault();
 
 		var id = $(this).attr('href'),
-			top = $(id).offset().top;
+			offset = $('nav').height(),
+			top = $(id).offset().top - offset;
 
-		$('body').animate({
+		$('html, body').animate({
 			scrollTop: top
-		}, 700)
+		}, 700);
+		
+		if ($('.menu-mobile-control').is(':visible')) {
+			$('.nav > .drop').slideUp(200, function(){
+				$('.menu-mobile-control').removeClass('active');
+			});
+		}
+
 	});
+
+	$('.menu-mobile-control').on('click', function() {
+    	var self = this,
+    		drop = $(this).parent().find('>.drop');
+    	if ( $(this).hasClass('active') ) {
+    		drop.slideUp(400, function(){
+    			$(self).removeClass('active');
+    		});
+    	} else {
+    		drop.slideDown(400, function() {
+    			$(self).addClass('active');
+    		});
+    	}
+    });
+
+    $('.account button').on('mouseenter', function() {
+    	$(this).css({'background-color': '#49cbcd'});
+    	$(this).parents('.account').find('.account-type').css({'background-color': '#49cbcd'});
+    }).on('mouseleave', function() {
+    	console.log($(this).parents('.account').find('.account-type'));
+    	$(this).css({'background-color': '#788492'});
+    	$(this).parents('.account').find('.account-type').css({'background-color': '#485460'});
+    });
+
 })
